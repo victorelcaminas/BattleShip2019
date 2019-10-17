ca1e9ffcd18765b7384866a649d25698184704c6
Victor Alonso <victor@ieselcaminas.org>
2019-10-17 18:50:00 +0200

    added remainingShots

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -9,10 +9,14 @@ public class BattleShip {
 	public static final int SUNK_SHIP_SYMBOL = 'X';
 	public static final int EMPTY_SYMBOL = '.';
 	
+	public static final int MAX_SHOTS = 30;
 	public static final int NUM_SHIPS = 10;
 	public static final int DIMENSION = 8;	
 	static char[][] matrix = new char[DIMENSION][DIMENSION];
 	static boolean gameOver;
+	
+	static int remainingShots;
+	static int sunkShipCounter;
 		
 	public static void main(String[] args) {
 		
@@ -21,6 +25,8 @@ public class BattleShip {
 		Scanner input = new Scanner(System.in);
 			     	     	
 		gameOver = false;
+		sunkShipCounter = 0;
+		remainingShots = MAX_SHOTS;
 	    initMatrix();
 	    addShipsToMatrix();
 	    	     
@@ -31,17 +37,28 @@ public class BattleShip {
 	    	System.out.println("Enter column (Number): ");
 	    	number = input.nextInt();
 	    	shoot(letter, number);
+	    	checkGameOver();	    	
 	    }
 	     
 	}
-	
+
+	private static void checkGameOver() {
+		if (sunkShipCounter >= NUM_SHIPS || remainingShots <= 0) {
+			gameOver = true;
+		}
+		
+	}
+
 	private static void shoot(char letter, int number) {
 		
 		int row = letter - 'A';
 		int col = number - 1;
 		
+		remainingShots --;
+		
 		if (matrix[row][col] == SHIP_SYMBOL) {
 			matrix[row][col] = SUNK_SHIP_SYMBOL;
+			sunkShipCounter ++;
 		} else {
 			if (matrix[row][col] == EMPTY_SYMBOL) {
 				matrix[row][col] = WATER_SYMBOL;
