07b1e47e2f8bcba9533952224afd911bcf50b8e9
Victor Alonso <victor@ieselcaminas.org>
2019-10-17 18:54:58 +0200

    added showResult()

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -39,7 +39,18 @@ public class BattleShip {
 	    	shoot(letter, number);
 	    	checkGameOver();	    	
 	    }
+	    
+	    showResult();
 	     
+	}
+
+	private static void showResult() {		
+		if (sunkShipCounter >= NUM_SHIPS) {
+			System.out.println("You WIN !!!!");
+		} else {
+			System.out.println("No remaining shots :-(");
+		}
+		
 	}
 
 	private static void checkGameOver() {
